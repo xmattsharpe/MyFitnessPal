b220fcc69efd267807f18e0d4e03b1f80b7ed5a6
Matthew Sharpe <mtshar74@thomasmore.edu>
2025-02-02 05:19:12 -0500

    Add project files.

---
+++ MyFitnessPal/Models/History.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFitnessPal.Models
+{
+    public class History
+    {
+        public int HistoryId { get; set; }
+        [Required]
+        public DateTime? HistoryDate { get; set; }
+        [Required]
+        public double? HistoryTime { get; set; }
+        [Required]
+        public string? HistoryCategory { get; set; }
+        [Required]
+        public string? HistoryDescription { get; set; }
+    }
+}
